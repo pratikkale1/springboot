f0637c8739e60b4bc65ec86a68b9214af796e009
pratikkale1 <119706086+pratikkale1@users.noreply.github.com>
2025-01-10 17:58:11 +0530

    Add files via upload

---
+++ 01-spring-boot-demo/src/test/java/com/luv2code/springboot/demo/mycoolaap/MycoolaapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolaapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
